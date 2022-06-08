9906c81e567b741ccd77eed5dba8d86d216498d9
sairamdgitte <sairamdgitte@gmail.com>
2022-06-08 13:47:37 +1000

    added console.log

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import { Timer } from './src/features/Timer';
 import { FocusHistory } from './src/features/FocusHistory';
 
 export default function App() {
+  console.log('yolo')
   const [currentSubject, setCurrentSubject] = useState();
   const [history, setHistory] = useState([])
   return (
